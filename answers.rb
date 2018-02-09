12a5e5a847f53888fd48b67ad818a8bc14a7ca91
Melissa Hill <mhill2040@gmail.com>
2018-02-09 16:44:27 -0700

    changes

---
+++ answers.rb
@@ -0,0 +1,22 @@
+def answers
+  [
+  "NO WAY" .colorize(:red),
+  "Get a beer" .colorize(:green),
+  "Yes".colorize(:green),
+  "Take a hike".colorize(:green),
+  "Outlook not so good".colorize(:red),
+  "Ask Later".colorize(:yellow),
+  "No".colorize(:red),
+  "Signs point to yes".colorize(:green),
+  "Take a nap".colorize(:green),
+  "You stress me out".colorize(:red),
+  "Don't count on it".colorize(:green),
+  "Doubt it".colorize(:red),
+  "Take a shot".colorize(:green),
+  "Cant tell you now".colorize(:yellow),
+  "Good luck with that".colorize(:red),
+  "Concentrate and ask again".colorize(:yellow),
+  "Seems like a bad idea".colorize(:red),
+  "Would your mom approve".colorize(:yellow),
+  ]
+end
