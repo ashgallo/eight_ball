37f01c6e94f40b0517b4a1f47292e726dfeaa2cb
Melissa Hill <mhill2040@gmail.com>
2018-02-10 19:48:36 -0700

    fixed indentation

--- answers.rb
+++ answers.rb
@@ -1,23 +1,24 @@
+#{ text: "NO WAY", color: :red },
 def answers
   [
-  "NO WAY" .colorize(:red),
-  "Get a beer" .colorize(:green),
-  "Yes".colorize(:green),
-  "Take a hike".colorize(:green),
-  "Outlook not so good".colorize(:red),
-  "Ask Later".colorize(:yellow),
-  "No".colorize(:red),
-  "Signs point to yes".colorize(:green),
-  "Take a nap".colorize(:green),
-  "You stress me out".colorize(:red),
-  "Don't count on it".colorize(:green),
-  "Doubt it".colorize(:red),
-  "Take a shot".colorize(:green),
-  "Cant tell you now".colorize(:yellow),
-  "Good luck with that".colorize(:red),
-  "Concentrate and ask again".colorize(:yellow),
-  "Seems like a bad idea".colorize(:red),
-  "Would your mom approve".colorize(:yellow),
+    "NO WAY"
+    "Get a beer"
+    "Yes"
+    "Take a hike"
+    "Outlook not so good"
+    "Ask Later"
+    "No"
+    "Signs point to yes"
+    "Take a nap"
+    "You stress me out"
+    "Don't count on it"
+    "Doubt it"
+    "Take a shot"
+    "Cant tell you now"
+    "Good luck with that"
+    "Concentrate and ask again"
+    "Seems like a bad idea"
+    "Would your mom approve"
   ]
   #TESTING
 end
